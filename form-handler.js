c055f452132f5199fef0f5805e4e1279f8dce2e8
carbar1000 <carlosbarradas.cripto@gmail.com>
2025-02-24 10:52:11 +0000

    enviando para o giti

--- form-handler.js
+++ form-handler.js
@@ -1,15 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('myForm');
     if (form) {
-<<<<<<< HEAD
-
-    form.removeEventListener('submit', handleSubmit);
-    form.addEventListener('submit', handleSubmit);
-
-=======
         // Remove event listeners existentes para evitar duplicação
         form.removeEventListener('submit', handleSubmit);
->>>>>>> a11efc6aab33e9bdcb59cb32ce7dcf20ca199362
         form.addEventListener('submit', handleSubmit);
     }
 });
@@ -21,13 +14,8 @@ function handleSubmit(event) {
     formData.forEach((value, key) => {
         dados[key] = value;
     });
-<<<<<<< HEAD
     console.log('Dados a serem enviados:', dados); // Adicionando log para verificar a estrutura dos dados
-=======
-    // Chama a função para enviar as respostas
->>>>>>> a11efc6aab33e9bdcb59cb32ce7dcf20ca199362
     enviarRespostas(dados);
-
 }
 
 async function enviarRespostas(dados) {
@@ -47,7 +35,6 @@ async function enviarRespostas(dados) {
 
         // Enviar para Google Sheets via webhook
         const responseSheets = await fetch(process.env.GOOGLE_SCRIPT_URL, {
-
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
