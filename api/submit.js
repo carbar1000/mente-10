5d826264ee603e45f2a7c9b6b262f542e7dfd6e8
carbar1000 <carlosbarradas.cripto@gmail.com>
2025-02-24 15:15:02 +0000

    enviando para o giti

--- api/submit.js
+++ api/submit.js
@@ -1,27 +1,32 @@
 import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
       return res.status(405).json({ message: 'Método não permitido' })
     }
 
-    const supabase = createClient(
-      process.env.SUPABASE_URL,
-      process.env.SUPABASE_ANON_KEY
-    )
-
     try {
-            if (!req.body || Object.keys(req.body).length === 0) {
-                return res.status(400).json({ error: 'Dados do formulário inválidos' });
-            }
-            const { data, error } = await supabase
-                .from('respostas')
-                .insert([req.body])
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).json({ error: 'Dados do formulário inválidos' });
+        }
+        
+        console.log('Dados recebidos para inserção:', req.body);
+        const { data, error } = await supabase
+            .from('respostas')
+            .insert([req.body]);
+        console.log('Dados inseridos com sucesso:', data);
 
+        if (error) {
+            console.error('Erro ao inserir dados no Supabase:', error);
+            throw error;
+        }
 
-      if (error) throw error
-
-      res.status(200).json({ success: true })
+        res.status(200).json({ success: true })
     } catch (error) {
       res.status(500).json({ error: error.message })
     }
